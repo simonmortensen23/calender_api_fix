297f9f1808ddfca0c0c2bb8de694715f80b22b50
simonmortensen23 <simon020393@gmail.com>
2022-10-19 10:34:24 +0000

    Add CLIENT_ORIGIN_DEV to settings.py

--- env.py
+++ env.py
@@ -4,3 +4,5 @@ os.environ['CLOUDINARY_URL'] = 'cloudinary://257161186376749:Cv8ugOCAFBDMO1fHBSd
 os.environ['DEV'] = '1'
 os.environ['SECRET_KEY'] = 'Key12345'
 os.environ['ALLOWED_HOST'] = 'kalender-planner-api.herokuapp.com'
+os.environ['CLIENT_ORIGIN'] = 'https://kalender-planner.herokuapp.com'
+os.environ['CLIENT_ORIGIN_DEV'] = 'https://3000-simonmorten-kalenderpla-tr6c7sdchr5.ws-eu71.gitpod.io'
