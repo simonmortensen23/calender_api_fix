ffda91719fb89048e947eb0eb49bb72aa64040c0
simonmortensen23 <simon020393@gmail.com>
2022-10-24 13:12:19 +0000

    Change in settings

--- calender_api/settings.py
+++ calender_api/settings.py
@@ -101,7 +101,7 @@ REST_FRAMEWORK = {
 }
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'restframework.renderers.JSONRenderer'
+        'rest_framework.renderers.JSONRenderer'
     ]
 
 
