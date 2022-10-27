40a757322350c699ab32df56587d9c58c68e17e1
simonmortensen23 <simon020393@gmail.com>
2022-10-27 20:16:43 +0000

    Create IsOwner permission

--- calender/views.py
+++ calender/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from calender_api.permissions import IsOwnerOrReadOnly
+from calender_api.permissions import IsOwnerOrReadOnly, IsOwner
 from .models import CalenderPost
 from .serializers import CalenderSerializer
 
@@ -9,7 +9,7 @@ from .serializers import CalenderSerializer
 # Create your views here.
 class CalenderList(generics.ListCreateAPIView):
     serializer_class = CalenderSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsOwner]
     queryset = CalenderPost.objects.all().order_by('-created_at')
 
     def perform_create(self, serializer):
--- calender_api/permissions.py
+++ calender_api/permissions.py
@@ -6,3 +6,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
         return obj.owner == request.user
 
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
