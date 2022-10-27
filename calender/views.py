9d7deac4d1e4740533569051c20875581caf577e
simonmortensen23 <simon020393@gmail.com>
2022-10-27 20:35:33 +0000

    create def get_queryset

--- calender/views.py
+++ calender/views.py
@@ -9,14 +9,18 @@ from .serializers import CalenderSerializer
 # Create your views here.
 class CalenderList(generics.ListCreateAPIView):
     serializer_class = CalenderSerializer
-    permission_classes = [IsOwner]
-    queryset = CalenderPost.objects.all().order_by('-created_at')
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        user = self.request.user
+        return CalenderPost.objects.filter(owner=user).order_by('-created_at')
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
+
 class CalenderDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsOwner]
     serializer_class = CalenderSerializer
     queryset = CalenderPost.objects.all().order_by('-created_at')
